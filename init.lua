8a017bf34f7b3b5b82c82c9e191bdc2d586229a1
Cy <whatever>
2013-10-05 19:23:07 -0700

    Added symlinks for the icons for sorting types.

--- init.lua
+++ init.lua
@@ -4,77 +4,98 @@
 
 function min(a,b)
     if a < b then
-        return a;
+        return a
     else
-        return b;
+        return b
     end
 end
 
 --------------------------------------------------------
 
+function sortInventory(inv,sorter) 
+    local tabl = inv:get_list("main")
+    if(tabl == nil) then
+        -- we don't sort furnaces!
+        return false
+    end
+
+    table.sort(tabl,sorter)
+    -- note take_item will set the name to '' when empty
+    -- NEVER reduce the inventory array size for chests
+    inv:set_list('main',tabl)
+    return true
+end
+
 sorters = {
     wise = function(a,b)
         if(a == nil) then
             if b == nil then
-                return true;
+                return true
             else
-                return false;
+                return false
             end
         elseif b == nil then
-            return true;
+            return true
         end
 
-        local aname = a:get_name();
-        local bname = b:get_name();
-        print('aname: '..aname..', bname: '..bname)
+        local aname = a:get_name()
+        local bname = b:get_name()
+        -- make sure empty slots go at the end
+
         if(string.len(aname) == 0) then
-            return false;
+            return false
         end
         if(string.len(bname) == 0) then
-            return true;
+            return true
         end
+
+        -- if the nodes are entirely different, sorted
         if (aname ~= bname) then
-            return aname < bname;
+            return aname < bname
         end
+
+        -- same node types
         -- may need to collapse the two together!
-        local bothmax = a:get_stack_max();
+        local bothmax = a:get_stack_max()
         if bothmax == 1 then
             -- it's unstackable                
-            local awear = a:get_wear();
-            local bwear = b:get_wear();
-            return awear < bwear;
+            local awear = a:get_wear()
+            local bwear = b:get_wear()
+            return awear < bwear
         end
-        local acount = a:get_count();
-        local bcount = b:get_count();
+        local acount = a:get_count()
+        local bcount = b:get_count()
         if(acount == bothmax) then
-            return true;
+            return true
         elseif (bcount == bothmax) then
-            return false;
+            return false
         end
-        local num = min(bcount,bothmax-acount);
-        a:add_item(b:take_item(num));
+        local num = min(bcount,bothmax-acount)
+        a:add_item(b:take_item(num))
         -- nothing can have both count AND wear, right?
-        return true;
+        -- now a:count > b:count so a should go first
+        return true
     end,
     amount = function(a,b)
-        return a:get_count() < b:get_count()
+        return a:get_count() > b:get_count()
     end,
     wear = function(a,b)
         return a:get_wear() < b:get_wear()
     end,
+    -- etc...
 }
 
 function registerWand(method,sorter)
     if method == nil then
-        name = "chest_sorter:wand";
-        sorter = sorters.wise;
-        assert(sorter ~= nil);
-        desc = 'Chest Sorter';
-        image = 'chest_sorter_wand.png';
+        name = "inventory_sorter:wand"
+        sorter = sorters.wise
+        assert(sorter ~= nil)
+        desc = 'Chest Sorter'
+        image = 'inventory_sorter_wand.png'
     else
-        name = "chest_sorter:wand_"..method;
-        desc = "Chest Sorter ("..method..')';
-        image = 'chest_sorter_wand_'..method..'.png'
+        name = "inventory_sorter:wand_"..method
+        desc = "Chest Sorter ("..method..')'
+        image = 'inventory_sorter_wand_'..method..'.png'
     end
 
     minetest.register_tool(name, {
@@ -87,42 +108,79 @@ function registerWand(method,sorter)
             max_drop_level=0
         },
         on_use = function(self,user,punched) 
-            local pos = minetest.get_pointed_thing_position(punched);
-            local meta = minetest.get_meta(pos);
-            local inv = meta:get_inventory();
+            local pos = minetest.get_pointed_thing_position(punched)
+            if pos==nil then
+                return
+            end
+            local meta = minetest.get_meta(pos)
+            local inv = meta:get_inventory()
             if(inv == nil) then
-                minetest.chat_send_player(user:get_player_name(),"That can't be sorted.","Sorter -!-");
-                return;
+                minetest.chat_send_player(user:get_player_name(),"That can't be sorted.","Sorter -!-")
+                return
             end
             -- this isn't exported, but default locked chest does this
-            local owner = meta:get_string("owner");
+            local owner = meta:get_string("owner")
             if(owner ~= nil and string.len(owner) ~= 0 and user:get_player_name() ~= owner) then
-                minetest.chat_send_player(user:get_player_name(),"That's not yours!","Sorter -!-");
-                return;
+                minetest.chat_send_player(user:get_player_name(),"That's not yours!","Sorter -!-")
+                return
             end
             -- Sokomine's shared chest locks
             if locks ~= nil and not locks:lock_allow_use(pos,user) then
-                minetest.chat_send_player(user:get_player_name(),"That's not yours!","Sorter -!-");
+                minetest.chat_send_player(user:get_player_name(),"That's not yours!","Sorter -!-")
             end
 
-            local tabl = meta:to_table()
-            inv = tabl["inventory"]["main"];
-            if(inv == nil) then
-                -- we don't sort furnaces!
-                return;
+            if(sortInventory(inv,sorter)) then
+                minetest.chat_send_player(user:get_player_name(),"Chest sorted.","Sorter -!-")
             end
-
-            table.sort(inv,sorter);
-            -- note take_item will set the name to '' when empty
-            -- NEVER reduce the inventory array size for chests
-            meta:from_table(tabl);
-            minetest.chat_send_player(user:get_player_name(),"Sorted.","Sorter -!-");
         end
-    });
+    })
 end
 
 for name,sorter in pairs(sorters) do
-    registerWand(name,sorter);
+    registerWand(name,sorter)
 end
 
-registerWand();
+registerWand()
+
+minetest.register_craft({
+    output = 'inventory_sorter:wand',
+    recipe = {
+        {'default:stick','default:stick',''},
+        {'default:stick','default:stick',''},
+        {'','default:stick',''}
+    }
+})
+
+-- is this one automatic...?
+
+minetest.register_craft({
+    output = 'inventory_sorter:wand',
+    recipe = {
+        {'','default:stick','default:stick'},
+        {'','default:stick','default:stick'},
+        {'','','default:stick'}
+    }
+})
+
+minetest.register_chatcommand('sort',{
+    params = '[optional algorithm: amount,wear]',
+    description = 'Sort your inventory! No takebacks.',
+    privs={interact=true},
+    func = function(name,param)
+        local sorter = sorters.wise;
+        if string.len(param) > 0 then
+            sorter = sorters[param];
+            if sorter == nil then
+                minetest.chat_send_player(name,"/sort [algorithm]","Sorter -!-")
+                minetest.chat_send_player(name,"Valid algorithms:","Sorter -!-")
+                for n,v in pairs(sorters) do
+                    minetest.chat_send_player(name,'    '..n,"Sorter -!-")
+                end
+                return;
+            end
+        end
+        if(sortInventory(minetest.get_player_by_name(name):get_inventory(),sorter)) then
+            minetest.chat_send_player(name,"Sorted.","Sorter -!-")
+        end
+    end
+})
